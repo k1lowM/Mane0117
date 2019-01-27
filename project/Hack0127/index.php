f4fd98ccc63407440d3b62ddd959450d92f92a7e
k1lowM <makiuchi.k1low@gmail.com>
2019-01-27 15:41:07 +0900

    Add files via upload
    ログインページを編集

--- project/Hack0127/index.php
+++ project/Hack0127/index.php
@@ -1,16 +1,65 @@
-<html>
-<head>
-</head>
-<body>
-
-	<form method="POST" action="main.php">
-
-		ログインID:<input type="text" name="name" size="40" /><br>
-		パスワード:<input type="password" name="password" size="40" /><br>
-		<input type="submit" value="ログイン" />
-
-	</form>
-
-</body>
-
+<!DOCTYPE html>
+<html lang="ja-jp">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href=https://stitches.hyperyolo.com/public/stitches.css rel="stylesheet">
+    <link href=https://fonts.googleapis.com/css?family=Muli:300,400,600,700,800,900 rel="stylesheet">
+    <link rel="stylesheet" href=https://use.fontawesome.com/releases/v5.6.3/css/all.css integrity=sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/ crossorigin="anonymous">
+    <title>まめラーメン(仮)</title>
+  </head>
+  <body><section class="font-sans h-screen w-full bg-cover text-center flex flex-col items-center justify-center" style="background:url(https://stitches-cdn.hyperyolo.com/demo-bg.png) no-repeat center;">
+  <div class="bg-white text-black rounded-full h-16 w-16 flex items-center justify-center mb-8"><i class="fas fa-play ml-1"></i></div>
+  <label for="tagline" class="uppercase tracking-extrawide text-white text-xs font-hairline mt-8">Watch Video</label>
+  <h3 class="text-white mx-auto max-w-sm mt-4 font-normal text-2xl leading-normal">リアル食べログ ラーメン編</h3>
+</section><section class="font-sans container max-w-xl m-auto flex flex-col lg:flex-row justify-center my-8">
+  <div class="order-2 lg:order-1 w-full lg:w-1/4 flex flex-col items-center lg:items-end justify-center text-center lg:text-right ml-0 lg:ml-8 mt-8">
+    <div class="w-12 h-12 rounded-full bg-grey-light mb-2" alt="Avatar of Jonathan Reinink"></div>
+    <h2 class="text-black mb-2 font-normal">ラーメン分析</h2>
+    <p class="text-grey-darker leading-normal mb-8">あなたの好きなラーメン屋を分析、グラフ化します</p>
+    <div class="w-12 h-12 rounded-full bg-grey-light mb-2" alt="Avatar of Jonathan Reinink"></div>
+    <h2 class="text-black mb-2 font-normal">あなたの好みのラーメン屋をとマッチング</h2>
+    <p class="text-grey-darker leading-normal mb-8">あたなの現在地と分析データから、好みのラーメン屋を見つけ出します</p>
+    <div class="w-12 h-12 rounded-full bg-grey-light mb-2" alt="Avatar of Jonathan Reinink"></div>
+    <h2 class="text-black mb-2 font-normal">趣味のあうラーメン好きとマッチング</h2>
+    <p class="text-grey-darker leading-normal">あたなの現在地と分析データから、好みがあうラーメン好きを見つけ出します</p>
+    </div>
+  <div class="order-1 lg:order-2 w-full lg:w-1/2 max-w-sm m-auto mb-6 lg:mb-0">
+    <img src="https://s3-us-west-2.amazonaws.com/968335633380.us-west-2.media/stitches/demo-phone.png" alt="">
+  </div>
+<!--
+  <div class="order-last w-full lg:w-1/4 flex flex-col items-center lg:items-start justify-center text-center lg:text-left mt-8 mr-8">
+    <div class="w-12 h-12 rounded-full bg-grey-light mb-2" alt="Avatar of Jonathan Reinink"></div>
+    <h2 class="text-black mb-2 font-normal">Cras iaculis ultricies nulla.</h2>
+    <p class="text-grey-darker leading-normal mb-8">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+    <div class="w-12 h-12 rounded-full bg-grey-light mb-2" alt="Avatar of Jonathan Reinink"></div>
+    <h2 class="text-black mb-2 font-normal">Praesent placerat risus quis eros.</h2>
+    <p class="text-grey-darker leading-normal mb-8">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+    <div class="w-12 h-12 rounded-full bg-grey-light mb-2" alt="Avatar of Jonathan Reinink"></div>
+    <h2 class="text-black mb-2 font-normal">Nunc dignissim risus id metus.</h2>
+    <p class="text-grey-darker leading-normal">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+    </div>
+</section><form class="font-sans text-sm rounded w-full max-w-md mx-auto my-8 px-8 pt-6 pb-8">
+  <div class="relative border rounded mb-4 shadow appearance-none label-floating">
+    <input class="w-full py-2 px-3 text-grey-darker leading-normal rounded" id="username" type="text" placeholder="Username">
+    <label class="absolute block text-grey-darker pin-t pin-l w-full px-3 py-2 leading-normal" for="username">
+      Username
+    </label>
+  </div>
+  <div class="relative border rounded mb-4 shadow appearance-none label-floating">
+    <input class="w-full py-2 px-3 text-grey-darker leading-normal rounded" id="password" type="password" placeholder="Password">
+    <label class="absolute block text-grey-darker pin-t pin-l w-full px-3 py-2 leading-normal" for="password">
+      Password
+    </label>
+  </div>
+  <div class="flex items-center justify-between">
+    <button class="bg-black hover:bg-black text-white py-2 px-4" type="button">
+    Sign In
+    </button>
+    <a class="inline-block align-baseline text-grey hover:text-grey-darker" href="#">
+      Forgot Password?
+    </a>
+  </div>
+</form></body>
 </html>
