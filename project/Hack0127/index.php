b6d4daffca25f374dc716628f8648a69b7892766
KazuMacAir <kaityo410@hotmail.com>
2019-01-27 18:21:35 +0900

    リンクを修正

--- project/Hack0127/index.php
+++ project/Hack0127/index.php
@@ -41,7 +41,7 @@
     <p class="text-grey-darker leading-normal">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
     </div>
 -->
-</section><form class="font-sans text-sm rounded w-full max-w-md mx-auto my-8 px-8 pt-6 pb-8">
+</section>
   <div class="relative border rounded mb-4 shadow appearance-none label-floating">
     <input class="w-full py-2 px-3 text-grey-darker leading-normal rounded" id="username" type="text" placeholder="Username">
     <label class="absolute block text-grey-darker pin-t pin-l w-full px-3 py-2 leading-normal" for="username">
@@ -56,11 +56,11 @@
   </div>
   <div class="flex items-center justify-between">
     <button class="bg-black hover:bg-black text-white py-2 px-4" type="button">
-	<a href="./main.php">Sign In</a>
+    <a href="main.php">Sign In</a>
     </button>
     <a class="inline-block align-baseline text-grey hover:text-grey-darker" href="#">
       Forgot Password?
     </a>
   </div>
-</form></body>
+</body>
 </html>
